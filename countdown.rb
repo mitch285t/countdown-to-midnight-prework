92fb7cf1941d7d5e439e80883b153b1e05e7152a
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-12 19:43:04 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,8 +9,8 @@ put "HAPPY NEW YEAR!"
 end
 def countdown_with_sleep(seconds)
   while seconds > 0 
-  seconds -+ 1 
-  puts "#{seconds} SECOND(S)!"
+   puts "#{seconds} SECOND(S)!"
+  seconds -= 1
   sleep(1)
 end
 put "HAPPY NEW YEAR!"
